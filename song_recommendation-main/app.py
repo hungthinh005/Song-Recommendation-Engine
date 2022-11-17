909ef3b6514d319fea02c2d19298352515e7f99b
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-17 22:32:17 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -72,8 +72,8 @@ def page():
         df_filter = df_filter_lyrics.loc[(df_filter_lyrics["lyrics"] == select_df)]
         
         
-#     st.sidebar.markdown("")
-    genre = st.sidebar.radio("*Choose your genre:*", genre_names, index=genre_names.index("Pop"))
+    st.sidebar.markdown("")
+    genre = st.sidebar.radio("Choose your genre:", genre_names, index=genre_names.index("Electronic"))
     
         
       
