1ee04a4521a5dff08ba4e9b7a66d54628f438d4b
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-19 14:48:50 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -62,7 +62,7 @@ def page():
         df_filter = df_filter_name.loc[(df_filter_name["name"] == select_df)]
         df_filter_uri = df_filter["uri"]
         df_filter_uri = df_filter_uri.values.tolist()
-        df_filter_by_artists = df_filter_name.loc[(df_filter_name["artists_name"] == df_filter["artists_name"])]
+        df_filter_by_artists = df_filter_name.loc[(df_filter_name["artists_name"] = df_filter["artists_name"])]
     if select_event == "By Lyrics":
         select_df = st.selectbox("Type Your Lyrics", df_filter_lyrics)
         df_filter = df_filter_lyrics.loc[(df_filter_lyrics["lyrics"] == select_df)]
