17fe85a9973b6cb830932c178d9591f0558dfc08
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-19 16:38:26 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -148,7 +148,7 @@ def page():
                 'Tempo',
                 0.0, 244.0, float(df_filter['tempo']))
     
-
+    st.markdown("Recommended Songs")
     tracks_per_page = 10
     test_feat = [acousticness, danceability, energy, instrumentalness, valence, tempo]
     uris, audios = n_neighbors_uri_audio(genre, start_year, end_year, test_feat)
