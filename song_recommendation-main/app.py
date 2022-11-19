8ff5ce0eb9b4a72cf3b776218565e40107ae9f50
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-19 15:47:29 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -81,7 +81,7 @@ def page():
       
         
     with st.container():
-        col1, col2 = st.columns((15,10))
+        col1, col2, col3 = st.columns((15,10,10))
         with col1:
             #test
 #             if df_filter_uri != null:
