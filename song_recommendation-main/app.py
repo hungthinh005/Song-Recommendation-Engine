b5a9e991c8dd3c77768571f0801a5bb956ebc40e
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-20 12:03:40 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -169,27 +169,20 @@ def page():
 
     if 'start_track_i' not in st.session_state:
         st.session_state['start_track_i'] = 0
+
+    if st.button("Next"):
+        st.session_state.counter += 1
+
+        components.html(
+            f"""
+                <p>{st.session_state.counter}</p>
+                <script>
+                    window.parent.document.querySelector('section.main').scrollTo(0, 0);
+                </script>
+            """,
+            height=0
+        )
     
-    st.title("Refresh Chart Data and Refocus at TOP")
-    my_slot1 = st.empty()
-    my_slot2 = st.empty()
-    my_slot3 = st.empty()
-    my_slot4 = st.empty()  
-
-    # Simulate reload data call
-    a = np.random.randn(20, 2)
-    b = np.random.randn(20, 2)
-    c = np.random.randn(20, 2)
-    d = np.random.randn(20, 2)
-    with st.spinner("Loading..."):
-        time.sleep(1)
-
-    my_slot1.line_chart(a)
-    my_slot2.line_chart(b)
-    my_slot3.line_chart(c)
-    my_slot4.line_chart(d)
-
-    st.button("Next")
     with st.container():
         col1, col2, col3 = st.columns([2,1,2])
         
