1ffba3dbf78dcc33facc6063c4a4440b01995700
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-24 23:30:33 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -136,7 +136,7 @@ def page():
             for i in df_filter_uri:
                 show_song = """<iframe src="https://open.spotify.com/embed/track/{}" width="260" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""".format(i)
                 components.html(show_song,height= 400)
-            st.markdown("**Advance**")
+            st.subtitle("Advance")
         with col2:
             st.markdown("**Genre:** ")
             st.markdown(df_filter_genre)
