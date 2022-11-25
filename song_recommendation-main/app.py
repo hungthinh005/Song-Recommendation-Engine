63088452ed519657ded22244a5b4bf5830a47654
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-25 10:25:01 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -218,7 +218,7 @@ def page():
                             r=audio[:5],
                             theta=audio_feats[:5]))
                             fig = px.line_polar(df_filter_name1, r='r', theta='theta', line_close=True)
-                            fig.update_layout(height=400, width=340)
+                            fig.update_layout(height=400, width=300)
                             st.plotly_chart(fig)
             
                 else:
