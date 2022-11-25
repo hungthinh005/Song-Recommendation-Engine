ec02ca20aa58f6dd56285a8aed1cab6405d32fc3
hungthinh005 <69240815+hungthinh005@users.noreply.github.com>
2022-11-25 15:57:18 +0700

    Update app.py

--- song_recommendation-main/app.py
+++ song_recommendation-main/app.py
@@ -202,7 +202,7 @@ def page():
     st.markdown("<div id='link_to_list'></div>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; color: white;'>---------------------------------------Your List---------------------------------------</h2>", unsafe_allow_html=True)   
     with st.container():
-        col1, col2, col3, col4 = st.columns([1.9,1.2,1.9,1.2])
+        col1, col2, col3, col4 = st.columns([1.7,1.2,1.7,1.2])
         
         current_tracks = tracks[st.session_state['start_track_i']: st.session_state['start_track_i'] + tracks_per_page]
         
